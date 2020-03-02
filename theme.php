620b67797dab76f71533854ab18586d4cb1606f2
James "n0cturne" Dumontier <dumontier.james@gmail.com>
2020-03-02 16:21:14 +0100

    Update theme.php

--- theme.php
+++ theme.php
@@ -14,8 +14,7 @@
 	<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
 	<?= $Wcms->css()?>
 
-    <link rel="stylesheet" href="<?= $Wcms->asset('css/devsec.css')?>">
-	<link rel="stylesheet" href="<?= $Wcms->asset('css/style.css')?>">
+	<link rel="stylesheet" href="<?= $Wcms->asset('style.css')?>">
 </head>
 
 <body>
